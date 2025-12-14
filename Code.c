6c1093507097d380cfdb05c3263217ec29b48188
kenta77 <ringowotaberarenai@gmail.com>
2025-12-14 19:10:10 +0900

    プログラム内容の変更

--- Code.c
+++ Code.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    printf("Hello, World!!\n");
+    int a = 70;
+    printf("変数aには70が入っています\n");
+
+    int* addrA = &a;
+    printf("変数aのアドレス: %p\n", addrA);
+    printf("%p番地に格納されている情報: %d\n", addrA, *addrA);
     return 0;
 }
