2b4739c97ba4ec3f3c20aa0b1b4616af5e12217f
kenta77 <ringowotaberarenai@gmail.com>
2025-12-28 22:37:47 +0900

    .gitignore追加とCファイル更新

--- code.c
+++ code.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int a = 70;
-    printf("変数aには70が入っています\n");
+typedef char String[1024];
 
-    int* addrA = &a;
-    printf("変数aのアドレス: %p\n", addrA);
-    printf("%p番地に格納されている情報: %d\n", addrA, *addrA);
+int main(void) 
+{
+    printf("(1)char型：%dバイト\n", (int)sizeof(char));
+    printf("(2)int型：%dバイト\n", (int)sizeof(int));
+    printf("(3)char*型：%dバイト\n", (int)sizeof(char*));
+    printf("(4)int*型：%dバイト\n", (int)sizeof(int*));
+
     return 0;
 }
