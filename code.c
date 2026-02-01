2458c836df4bdcfdb1b67d5a2efb59fb2b6f0f01
kenta77 <ringowotaberarenai@gmail.com>
2026-02-01 18:16:57 +0900

    2026/02/02 ダンジョン

--- code.c
+++ code.c
@@ -1,17 +1,62 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-int main(void)
+/*** 列挙型宣言 ***/
+
+/*** グローバル定数の宣言 ***/
+
+/*** 構造体型宣言 ***/
+
+/*** プロトタイプ宣言 ***/
+
+int goDungeon(char* playerName);
+int doBattle(char* playerName, char* enemyName);
+
+/*** 関数宣言 ***/
+
+int main(int argc, char** argv)
 {
-    char a[] = "hello";
-    char b[] = {0, 1, 2, 3, 4, 5};
-    char* heap = calloc(10, sizeof(char));
-    char* c = heap + 4;
+    if(argc != 2) {
+        printf("エラー: プレイヤー名を指定して起動してください。\n");
+        return 1;
+    }
 
-    c[0] = 'S';
-    c[1] = 'C';
+    printf("*** Puzzle & Monsters ***\n");
 
-    free(heap);
+    // いざ、ダンジョンへ
+    int winCount = goDungeon(argv[1]);
+
+    // 冒険終了後
+    if(winCount == 5) {
+        printf("***GAME CLEAR!***\n");
+    } else {
+        printf("***GAME OVER***\n");
+    }
+    printf("倒したモンスター数=%d\n", winCount);
     return 0;
 }
+
+// (2) ダンジョン開始から終了までの流れ
+int goDungeon(char* playerName) 
+{
+    printf("%sはダンジョンに到着した\n", playerName);
+
+    int winCount = 0;
+    winCount += doBattle(playerName, "スライム");
+    winCount += doBattle(playerName, "ゴブリン");
+    winCount += doBattle(playerName, "オオコウモリ");
+    winCount += doBattle(playerName, "ウェアウルフ");
+    winCount += doBattle(playerName, "ドラゴン");
+    printf("%sはダンジョンを制覇した！\n", playerName);
+    return winCount;
+}
+
+// (3) バトル開始から終了までの流れ
+int doBattle(char* playerName, char* enemyName)
+{
+    printf("%sが現れた！\n", enemyName);
+    // ダミーのため速攻倒す
+    printf("%sを倒した！\n", enemyName);
+    return 1;
+}
+
+/*** ユーティリティ関数宣言 ***/
